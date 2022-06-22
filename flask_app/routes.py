f0e74bafaf42a067dcb15e15aaf26af9e3c0b2d2
ghamutaz <42781525+avenzi@users.noreply.github.com>
2022-06-22 14:40:11 -0400

    updated hello world page

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -2,9 +2,7 @@
 from flask import current_app as app
 @app.route('/')
 def hello():
-	count = 25
 	return f"""<html>
 	           <h1>Hello World!</h1>
-	           <p>My favorite number is {count}.</p>
 	           </html>
 	        """
